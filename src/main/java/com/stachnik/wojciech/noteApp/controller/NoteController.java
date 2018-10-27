ec707d4865304e189b6e61d174f18fc0d82f98a6
WojciechStachnik <wojciech.stachnik@gmail.com>
2018-10-27 14:49:32 +0200

    repair NoteController.deleteNote

--- src/main/java/com/stachnik/wojciech/noteApp/controller/NoteController.java
+++ src/main/java/com/stachnik/wojciech/noteApp/controller/NoteController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class NoteController {
     @Autowired
     private NoteRepository noteRepository;
@@ -47,8 +46,9 @@ public class NoteController {
     }
 
     //delete note by id
-    @PostMapping("/notes/{id}")
+    @DeleteMapping("/notes/{id}")
     public ResponseEntity<?> deleteNote(@PathVariable(value = "id")Long noteId){
+        Note note = noteRepository.findById(noteId).orElseThrow(()-> new ResourceNotFoundException("Note", "id", noteId));
         return ResponseEntity.ok().build();
     }
 }
