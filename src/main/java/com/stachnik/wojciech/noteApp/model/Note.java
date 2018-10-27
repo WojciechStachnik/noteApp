156ba7ce4164ad801450aa0dbe0ced1b4caea5a2
WojciechStachnik <wojciech.stachnik@gmail.com>
2018-10-27 11:59:16 +0200

    model Note create

---
+++ src/main/java/com/stachnik/wojciech/noteApp/model/Note.java
@@ -0,0 +1,43 @@
+package com.stachnik.wojciech.noteApp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table (name="notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
